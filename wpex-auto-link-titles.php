a61819d606e4f9e4809bdfd3cc168178380779a1
WPExplorer <wpexplorer@gmail.com>
2015-07-19 14:57:37 -0700

    Update wpex-auto-link-titles.php

--- wpex-auto-link-titles.php
+++ wpex-auto-link-titles.php
@@ -19,39 +19,67 @@ if ( is_admin() ) {
 // This is the main plugin function - does everything.
 function wpex_auto_add_link_titles( $content ) {
 
-	// No need to do anything if there isn't any content
-	if ( empty( $content ) ) {
+	// No need to do anything if there isn't any content or if DomDocument isn't supported
+	if ( empty( $content ) || ! class_exists( 'DOMDocument' ) ) {
 		return;
 	}
 
 	// Define links array
 	$links = array();
 
-	// Get page content
-	$html = new DomDocument;
-	$html->loadHTML( $content ); 
-	$html->preserveWhiteSpace = false;
+	// Create new dom object
+	$dom = new DOMDocument;
 
-	// Loop through all links
-	foreach( $html->getElementsByTagName( 'a' ) as $link ) {
+	// Load html into the object
+	$dom->loadHTML( $content );
+
+	// Discard white space
+	$dom->preserveWhiteSpace = false;
+
+	// Loop through all content links
+	foreach( $dom->getElementsByTagName( 'a' ) as $link ) {
+
+		// If the title attribute is already defined no need to do anything
+		if ( ! empty( $link->getAttribute( 'title' ) ) ) {
+			continue;
+		}
 
 		// Get link text
-		$link_text = ! empty( $link->textContent ) ? $link->textContent : '';
+		$link_text = $link->textContent;
+
+		// If there isn't any link text (most probably an image) lets try and get it from the first child
+		if ( ! $link_text && ! empty( $link->firstChild ) && $link->firstChild->hasAttributes() ) {
+
+			// Get alt
+			$alt = $link->firstChild->getAttribute( 'alt' );
+
+			// If no alt get image title
+			$alt = $alt ? $alt : $link->firstChild->getAttribute( 'title' );
+
+			// Clean up alt (remove dashses and underscores which are common in WP)
+			$alt = str_replace( '-', ' ', $alt );
+			$alt = str_replace( '_', ' ', $alt );
+
+			// Return cleaned up alt
+			$link_text = $alt;
+
+		}
 
 		// Save links and link text in $links array
-		if ( empty( $link->getAttribute( 'title' ) ) ) {
+		if ( $link_text ) {
 			$links[$link_text] = $link->getAttribute( 'href' );
 		}
 
 	}
 
-	// Loop through links and update post content to add link titles
+	// Loop through links array and update post content to add link titles
 	if ( ! empty( $links ) ) {
 		foreach ( $links as $text => $link ) {
 			if ( $link && $text ) {
-				$text    = ucwords( $text );
-				$replace = $link .'" title="'. $text .'"';
-				$content = str_replace( $link .'"', $replace, $content );
+				$text    = esc_attr( $text ); // Sanitize
+				$text    = ucwords( $text );  // Captilize words (looks better imo)
+				$replace = $link .'" title="'. $text .'"'; // Add title to link
+				$content = str_replace( $link .'"', $replace, $content ); // Replace post content
 			}
 
 		}
